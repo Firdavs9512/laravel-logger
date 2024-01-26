cf4dd7b44ca7a656712ae552b487d7c06df5381e
Nobody9512 <89482141+Nobody9512@users.noreply.github.com>
2024-01-26 17:56:37 +0300

    Update Log.php
    Add docs in Log model

--- src/Models/Log.php
+++ src/Models/Log.php
@@ -8,5 +8,28 @@ class Log extends Model
 {
     protected $table = 'logs';
 
-    protected $guarded = [];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'action',
+        'model',
+        'model_id',
+        'old_value',
+        'new_value',
+        'user_id',
+        'ip',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'old_value' => 'array',
+        'new_value' => 'array',
+    ];
 }
